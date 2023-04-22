2cda0360f5c8208ee468d6c7149dae9f33801fe4
Replit user <>
2023-04-22 23:46:10 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -119,7 +119,7 @@ Pair * nextTreeMap(TreeMap * tree) {
         else{
           while(tree->current<=aux){
             tree->current = tree->current->parent;
-            printf("a");
+            printf(tree->current->pair->key);
             if(tree->current==tree->root)
               return NULL;
           }  
