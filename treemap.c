f9425d51ec7862eb5ef30b2e35839ece473743f2
Replit user <>
2023-04-23 03:11:45 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -109,7 +109,10 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 
 
 Pair * upperBound(TreeMap * tree, void* key) {
-    return NULL;
+  Pair * p = searchTreeMap(tree, key);
+  
+  
+  return p;
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
