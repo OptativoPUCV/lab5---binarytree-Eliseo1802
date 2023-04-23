bd80b99fa1661c8a8b9b20403208416500757708
Replit user <>
2023-04-23 00:26:19 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -118,7 +118,7 @@ Pair * nextTreeMap(TreeMap * tree) {
         if((int*)tree->current->pair->key > (int*)aux->pair->key)
           return tree->current->pair;
         else{
-          while(lower_than_int(tree->current->pair->key, aux->pair->key)
+          while(lower_than(tree->current->pair->key, aux->pair->key)
             || is_equal(tree,tree->current->pair->key, aux->pair->key)){
           //while((int*)tree->current->pair->key <= (int*)aux->pair->key){
             tree->current = tree->current->parent;
